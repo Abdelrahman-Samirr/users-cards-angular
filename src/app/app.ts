3f738c20eb73e0ffe7d4d1059fbcc3f44dba6f6e
Abdelrahman-Samirr <abdelrahmansamir641@gmail.com>
2025-06-17 16:30:08 +0300

    complete design using Control flowand Directives

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Cards } from './cards/cards';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Cards],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
